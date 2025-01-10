1568bbcba61c91f79bc1fd16a5b8011017ba8647
yauheniyastrelkova <yauheniya.strelkova@avocodo.com>
2025-01-10 15:52:49 +0100

    add utils project

---
+++ Common/Utils/SerializationHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+
+namespace Common.Utils;
+
+public class SerializationHelper
+{
+    public static T Deserialize<T>(string json)
+    {
+        var options = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true,
+        };
+        
+        return JsonSerializer.Deserialize<T>(json, options)!;
+    }
+}
